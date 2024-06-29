97b872af66acb9c204f23eea2766aaede19f49bd
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2024-06-29 05:30:57 +0530

    Analytics API's

---
+++ packages/nextjs/app/api/analytics/total_interactions/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import Analytics from "~~/model/analytics";
+import connectDB from "~~/services/connectDB";
+
+type DateFilter = {
+  $gte?: Date;
+  $lte?: Date;
+};
+
+export async function GET(request: NextRequest) {
+  await connectDB();
+  const { searchParams } = new URL(request.url);
+  const startDate = searchParams.get("startDate");
+  const endDate = searchParams.get("endDate");
+  const frameId = searchParams.get("frameId");
+  const journeyId = searchParams.get("journeyId");
+  const dateFilter: DateFilter = {};
+  if (startDate) {
+    dateFilter.$gte = new Date(startDate);
+  }
+  if (endDate) {
+    dateFilter.$lte = new Date(endDate);
+  }
+  const query: any = {};
+  if (startDate || endDate) {
+    query.createdAt = dateFilter;
+  }
+  if (frameId) {
+    query.frameId = frameId;
+  }
+  if (journeyId) {
+    query.journeyId = journeyId;
+  }
+  const totalUsers = await Analytics.countDocuments(query);
+  return new NextResponse(JSON.stringify({ totalUsers: totalUsers }), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
---
+++ packages/nextjs/app/api/analytics/unique_fids/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import Analytics from "~~/model/analytics";
+import connectDB from "~~/services/connectDB";
+
+type DateFilter = {
+  $gte?: Date;
+  $lte?: Date;
+};
+
+export async function GET(request: NextRequest) {
+  await connectDB();
+  const { searchParams } = new URL(request.url);
+  const startDate = searchParams.get("startDate");
+  const endDate = searchParams.get("endDate");
+  const frameId = searchParams.get("frameId");
+  const journeyId = searchParams.get("journeyId");
+  const dateFilter: DateFilter = {};
+  if (startDate) {
+    dateFilter.$gte = new Date(startDate);
+  }
+  if (endDate) {
+    dateFilter.$lte = new Date(endDate);
+  }
+  const query: any = {};
+  if (startDate || endDate) {
+    query.createdAt = dateFilter;
+  }
+  if (frameId) {
+    query.frameId = frameId;
+  }
+  if (journeyId) {
+    query.journeyId = journeyId;
+  }
+  const distinctUsers = await Analytics.distinct("fid", query);
+  return new NextResponse(JSON.stringify({ uniqueFids: distinctUsers.length }), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
